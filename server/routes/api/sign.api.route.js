64b95d5371fcabcfb894366f57406404e015685a
MaryFBakeer <mariabefrank@gmail.com>
2024-04-11 18:35:06 +0300

    регалога с проверками

--- server/routes/api/sign.api.route.js
+++ server/routes/api/sign.api.route.js
@@ -3,70 +3,62 @@ const bcrypt = require('bcrypt');
 const signUtils = require('../../utils/signUtils');
 const jwtConfig = require('../../config/jwtConfig');
 const { User } = require('../../db/models');
+const { Op } = require('sequelize');
 
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.findAll();
-    const response = { message: 'success', users };
-    res.json(response);
-  } catch ({ message }) {
-    res.json({ message });
-  }
-});
-router.put('/plus/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { cost } = req.body;
-    const user = await User.findOne({ where: { id } });
-    const newScores = +user.scores + +cost;
-    await User.update({ scores: newScores }, { where: { id } });
-    const response = { message: 'success', scores: newScores };
-    res.json(response);
-  } catch ({ message }) {
-    res.json({ message });
-  }
-});
-router.put('/minus/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { cost } = req.body;
-    const user = await User.findOne({ where: { id } });
-    const newScores = +user.scores - +cost;
-    await User.update({ scores: newScores }, { where: { id } });
-    const response = { message: 'success', scores: newScores };
-    res.json(response);
-  } catch ({ message }) {
-    res.json({ message });
-  }
-});
+// router.get('/', async (req, res) => {
+//   try {
+//     const users = await User.findAll();
+//     const response = { message: 'success', users };
+//     res.json(response);
+//   } catch ({ message }) {
+//     res.json({ message });
+//   }
+// });
 
-router.post('/in', async (req, res) => {
+router.post('/authorization', async (req, res) => {
+  let user;
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+
     if (email.trim() === '' || password.trim() === '') {
       res.json({ message: 'Заполните поля корректно' });
       return;
     }
-    if (email.trim().length !== email.length || email.replace(' ', '').length !== email.length) {
+    if (
+      email.trim().length !== email.length ||
+      email.replace(' ', '').length !== email.length
+    ) {
       res.json({ message: 'E-mail не должен содержать пробелов' });
       return;
     }
-    if (password.trim().length !== password.length || password.replace(' ', '').length !== password.length) {
+    if (
+      password.trim().length !== password.length ||
+      password.replace(' ', '').length !== password.length
+    ) {
       res.json({ message: 'Пароль не должен содержать пробелов' });
       return;
     }
+
+    const user = await User.findOne({
+      where: {
+        [Op.or]: [{ email }, { login: email }],
+      },
+      attributes: ['id', 'email', 'login'],
+    });
+
     if (!user) {
-      res.json({ message: 'Такого пользователя не существует' });
+      res.json({ message: 'Такого пользователя нет или пароль неверный' });
       return;
     }
     const passTest = await bcrypt.compare(password, user.password);
     if (!passTest) {
-      res.json({ message: 'Пароль не совпадает' });
+      res.json({ message: 'Такого пользователя нет или пароль неверный' });
     } else {
       const { accessToken, refreshToken } = signUtils({
         user: {
-          id: user.id, email: user.email, name: user.name, role: user.role,
+          id: user.id,
+          email: user.email,
+          login: user.login,
         },
       });
       res.locals.user = user;
@@ -87,12 +79,10 @@ router.post('/in', async (req, res) => {
   }
 });
 
-router.post('/up', async (req, res) => {
+router.post('/registration', async (req, res) => {
   try {
-    const {
-      email, password, r_password, name,
-    } = req.body;
-    const user = await User.findOne({ where: { email } });
+    const { email, password, r_password, login } = req.body;
+
     if (email.trim() === '' || password.trim() === '') {
       res.json({ message: 'Заполните поля корректно' });
       return;
@@ -103,33 +93,58 @@ router.post('/up', async (req, res) => {
       res.json({ message: 'Некорректно заполнен e-mail' });
       return;
     }
-    if (password.trim().length !== password.length || password.replace(' ', '').length !== password.length || r_password.trim().length !== r_password.length || r_password.replace(' ', '').length !== r_password.length) {
-      res.json({ message: 'Пароль или повтор пароля не должен содержать пробелов' });
+    const invalidCharacters = /[@#$%^&*()-_+=|\\:;"'<,>.?/~]/;
+    if (
+      login.trim().length !== login.length ||
+      login.replace(' ', '').length !== login.length ||
+      invalidCharacters.test(login)
+    ) {
+      res.json({ message: 'Login не должен содержать пробелов или специальных символов' });
       return;
     }
-    if (user) {
-      res.json({ message: 'Такой e-mail занят' });
+
+    if (
+      password.trim().length !== password.length ||
+      password.replace(' ', '').length !== password.length ||
+      r_password.trim().length !== r_password.length ||
+      r_password.replace(' ', '').length !== r_password.length
+    ) {
+      res.json({
+        message: 'Пароль или повтор пароля не должен содержать пробелов',
+      });
       return;
     }
+
+    const userEmail = await User.findOne({ where: { email } });
+    if (userEmail) {
+      res.json({ message: 'Пользователь с таким email уже существует!' });
+      return;
+    }
+
+    const userLogin = await User.findOne({ where: { login } });
+    if (userLogin) {
+      res.json({ message: 'Пользователь с таким login уже существует!' });
+      return;
+    }
+
     if (password !== r_password) {
-      res.json({ message: 'Пароль не совпадает' });
+      res.json({ message: 'Пароли не совпадают!' });
     } else {
       const newUser = await User.create({
         email,
         password: await bcrypt.hash(password, 10),
-        name,
-        scores: 0,
+        login,
         createdAt: new Date(),
         updatedAt: new Date(),
       });
 
-      const { accessToken, refreshToken } = signUtils({
-        user: {
-          id: newUser.id, email: newUser.email, name: newUser.name,
-        },
+      const user = await newUser.findOne({
+        where: { id: newUser.id },
+        attributes: ['id', 'email', 'login'],
       });
+      const { accessToken, refreshToken } = signUtils({ user });
 
-      res.locals.user = newUser;
+      res.locals.user = user;
       res
         .cookie(jwtConfig.refresh.type, refreshToken, {
           maxAge: jwtConfig.refresh.expiresIn,
@@ -140,40 +155,34 @@ router.post('/up', async (req, res) => {
           httpOnly: true,
         });
 
-      res.json({ message: 'success', user: newUser });
+      res.json({ message: 'success', user: user });
     }
   } catch ({ message }) {
     res.json({ message });
   }
 });
-
-router.get('/out', async (req, res) => {
+router.get('/logout', (req, res) => {
   try {
-    const user = {
-      id: 0,
-      email: '',
-      password: '',
-      name: '',
-      createdAt: '',
-      updatedAt: '',
-    };
-    res
-      .clearCookie(jwtConfig.access.type)
-      .clearCookie(jwtConfig.refresh.type);
-    res.json({ message: 'success', user });
+    res.clearCookie(configJWT.access.type).clearCookie(configJWT.refresh.type);
+    res.json({ message: 'success' });
   } catch ({ message }) {
-    console.log(message);
+    res.json({ message });
   }
 });
 
 router.get('/check', async (req, res) => {
   try {
     if (res.locals.user) {
-      const user = await User.findOne({ where: { id: res.locals.user.id } });
+      const user = await User.findOne({
+        where: { id: res.locals.user.id },
+        attributes: ['id', 'email', 'login'],
+      });
       res.json({ user });
+      return;
     }
+    res.json({ message: 'Сначала войдите в свой аккаунт' });
   } catch ({ message }) {
-    res.json(message);
+    res.json({ message });
   }
 });
 
