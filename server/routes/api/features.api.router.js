df6ef995e98b8f3c6b9c0591f8f07f170e06bafb
LebedevD174 <Lebedevdima174@yandex.ru>
2024-04-12 12:13:22 +0300

    correct server

--- server/routes/api/features.api.router.js
+++ server/routes/api/features.api.router.js
@@ -26,7 +26,7 @@ router.get('/', async (req, res) => {
             attributes: ['id', 'title', 'description', 'img', 'user_id'],
             include: [
               { model: Formula,
-                attributes: ['id', 'cocktail_id', 'barware_id', 'drink_id', 'drinks_volume', 'tech_id', 'ingredient_id', 'ingredients_volume', 'order'],
+                attributes: ['id', 'cocktail_id', 'barware_id', 'drink_id', 'drinks_volume', 'tech_id', 'ingredient_id', 'ingredient_volume', 'order'],
                 include: [
                   {
                     model: Barware,
@@ -78,7 +78,7 @@ router.get('/:id', async (req, res) => {
             attributes: ['id', 'title', 'description', 'img', 'user_id'],
             include: [
               { model: Formula,
-                attributes: ['id', 'cocktail_id', 'barware_id', 'drink_id', 'drinks_volume', 'tech_id', 'ingredient_id', 'ingredients_volume', 'order'],
+                attributes: ['id', 'cocktail_id', 'barware_id', 'drink_id', 'drinks_volume', 'tech_id', 'ingredient_id', 'ingredient_volume', 'order'],
                 include: [
                   {
                     model: Barware,
