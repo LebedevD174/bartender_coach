9a008ba3ceb67608b49c3e893c60fe288197db28
LebedevD174 <Lebedevdima174@yandex.ru>
2024-04-14 15:22:02 +0300

    correct

--- client/src/components/Coach/CoachWindow.tsx
+++ client/src/components/Coach/CoachWindow.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 function CoachWindow() {
+    const 
     return (
         <div>
-            
+            {}
         </div>
     );
 }
--- client/src/components/Cocktails/types/cocktail.ts
+++ client/src/components/Cocktails/types/cocktail.ts
@@ -1,3 +1,5 @@
+import type { Drink } from "../../Drinks/types/drink";
+
 export type Cocktail = {
   id: number;
   title: string;
@@ -14,6 +16,35 @@ export type CocktailType = {
   error: string | undefined;
 };
 
-export type CocktailInclude = Cocktail & {
-  
+export type Formula = {
+  id: number;
+  cocktail_id: number;
+  barware_id: number;
+  drink_id: number;
+  drinks_volume: number;
+  tech_id: number,
+  ingredient_id: number,
+  ingredient_volume: number,
+  order: number,
+  Barware: Barware,
+  Ingredient: Ingredient,
+  Drink: Drink,
+}
+
+export type Barware = {
+  id: number,
+  title: string,
+  description: string,
+  img: string,
+}
+
+export type Ingredient = {
+  id: number,
+  title: string,
+  measure: string,
+  img: string,
+}
+
+export type CocktailFormula = Cocktail & {
+  Formula: Formula[]
 }
