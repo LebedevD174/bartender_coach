fdee7864ccb12e175e18ba52ca7c7129ad861095
LebedevD174 <Lebedevdima174@yandex.ru>
2024-04-15 15:37:34 +0300

    correct coach

--- client/src/components/Coach/CoachWindow.tsx
+++ client/src/components/Coach/CoachWindow.tsx
@@ -1,33 +1,74 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { DragDropContext, Droppable, Draggable, type DropResult } from 'react-beautiful-dnd';
+import {shallowEqual as equal} from 'shallow-equal'; // Assuming you install shallow-equal
 import type { RootState } from '../../app/redux/store';
 import { useAppSelector } from '../../app/redux/store';
 import type { CocktailFormula, Formula } from '../Cocktails/types/cocktail';
 import { Cocktail } from '../Cocktails/types/cocktail';
-import {shallowEqual as equal} from 'shallow-equal'; // Assuming you install shallow-equal
 
 function CoachWindow({ cocktail, setCocktail }: { cocktail: CocktailFormula | null, setCocktail: (el: CocktailFormula | null) => void }): JSX.Element {
   const user = useAppSelector((store: RootState) => store.auth.user);
   const profile = useAppSelector((store: RootState) => store.profile.profile);
 //   const [formulas, setFormulas] = useState(cocktail.Formulas.toSorted((a: Formula, b: Formula) => a.order - b.order));
-  const [formulas, setFormulas] = useState(cocktail.Formulas.toSorted((a: Formula, b: Formula) => Math.random() - 0.5));
-  const initialFormulas = useRef(cocktail.Formulas.toSorted((a: Formula, b: Formula) => a.order - b.order));
+  const [formulas, setFormulas] = useState<Formula[]>(cocktail.Formulas.toSorted((a: Formula, b: Formula) => Math.random() - 0.5));
+  const [newFormulas, setNewFormulas] = useState<Formula[]>([]);
+  const initialFormulas = useRef<Formula[]>(cocktail.Formulas.toSorted((a: Formula, b: Formula) => a.order - b.order));
+  const [check, setCheck] = useState({});
   const [result, setResult] = useState(false);
 
   useEffect(()=>{
-    if (equal(formulas, initialFormulas.current)) {
+    // console.log(initialFormulas.current.map(el=>el.order));
+    // console.log(newFormulas.map(el=>el.order));
+    // console.log(formulas.map(el=>el.order));
+    initialFormulas.current.forEach((formula, index) => {
+        console.log(check);
+        
+        if (newFormulas[index]){
+            console.log(newFormulas[index].order === formula.order);
+            if (formula.order === newFormulas[index].order){
+                
+            }
+            
+        }
+    })
+    if (equal(newFormulas, initialFormulas.current)) {
         setResult(true)
     } else {
         setResult(false)
     }
-  })
-  const onDragEnd = (result: DropResult): void => {
+
+  },[newFormulas])
+  useEffect(()=>{
+    setFormulas(cocktail.Formulas.toSorted((a: Formula, b: Formula) => Math.random() - 0.5))
+    setNewFormulas([])
+    setCheck({...Array(formulas.length).fill(null)})
+  }, [cocktail])
+//   const onDragEnd = (result: DropResult): void => {
+//     if (!result.destination) return;
+//     const items:Formula[] = Array.from(formulas);
+//     const [reorderedItem] = items.splice(result.source.index, 1);
+//     items.splice(result.destination.index, 0, reorderedItem);
+//         setFormulas(items);
+//   };
+
+const onDragEnd = (result: DropResult): void => {
     if (!result.destination) return;
-    const items:Formula[] = Array.from(formulas);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-        setFormulas(items);
-  };
+   
+    const items: Formula[] = Array.from(formulas);
+    const newItems: Formula[] = Array.from(newFormulas);
+   
+    if (result.source.droppableId === "droppable" && result.destination.droppableId === "newDroppable") {
+       const [reorderedItem] = items.splice(result.source.index, 1);
+       newItems.push(reorderedItem);
+       setFormulas(items);
+       setNewFormulas(newItems);
+    } else if (result.source.droppableId === "newDroppable" && result.destination.droppableId === "droppable") {
+       const [reorderedItem] = newItems.splice(result.source.index, 1);
+       items.push(reorderedItem);
+       setNewFormulas(newItems);
+       setFormulas(items);
+    }
+   };
 
   function closeCoach(): void {
     setCocktail(null)
@@ -66,6 +107,30 @@ function CoachWindow({ cocktail, setCocktail }: { cocktail: CocktailFormula | nu
             </div>
           )}
         </Droppable>
+        <Droppable droppableId="newDroppable">
+ {(provided) => (
+    <div {...provided.droppableProps} ref={provided.innerRef} style={{ backgroundColor: 'lightgrey', padding: '10px', borderRadius: '5px' }}>
+      <h3>Коктейль</h3>
+      {newFormulas.map((formula: Formula, index: number) => (
+        <Draggable key={formula.id} draggableId={`draggable-${formula.id}`} index={index}>
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} style={{color:'black', margin: '5px 0', padding: '5px', border: '1px solid #ccc', borderRadius: '5px' }}>
+              {/* Отображение информации о формуле */}
+              {formula.order && <div>{formula.order}</div>}
+              {formula.Barware && <div>{formula.Barware?.title}</div>}
+              {formula.Drink && <div>{formula.Drink?.title}: {formula.drinks_volume}мл</div>}
+              {formula.Ingredient && (
+                <div>{formula.Ingredient?.title}: {formula.ingredient_volume} {formula.Ingredient?.measure}</div>
+              )}
+              {formula.Tech && <div>{formula.Tech?.title}</div>}
+            </div>
+          )}
+        </Draggable>
+      ))}
+      {provided.placeholder}
+    </div>
+ )}
+</Droppable>
       </DragDropContext>
 
       <div>
