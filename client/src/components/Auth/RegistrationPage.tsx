1ed37726f900238265138c308f1bd5fe1c2bd5b0
MaryFBakeer <mariabefrank@gmail.com>
2024-04-16 20:28:20 +0300

    красотааааа логиРеги

--- client/src/components/Auth/RegistrationPage.tsx
+++ client/src/components/Auth/RegistrationPage.tsx
@@ -7,111 +7,110 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../app/redux/store';
-import {userReg} from './authSlice'
+import { userReg } from './authSlice';
 import Modal from '../ui/Modal';
-import * as thunk from './authSlice'
+import * as thunk from './authSlice';
 
 function RegistrationPage(): JSX.Element {
   const [login, setLogin] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [checkPassword, setCheckPassword] = useState('');
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const user = useAppSelector((store) => store.auth.user)
-  const error = useAppSelector((state) => state.auth.error)
-  const navigate = useNavigate()
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const user = useAppSelector((store) => store.auth.user);
+  const error = useAppSelector((state) => state.auth.error);
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  const onHandleSubmit = async (
-    e: React.FormEvent<HTMLFormElement>
-  ): Promise<void> => {
+  const onHandleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
     const data = {
       login,
       email,
       password,
-      checkPassword
+      checkPassword,
     };
-        dispatch(userReg(data))
-      }
+    dispatch(userReg(data));
+  };
 
-    const closeModal = () :void => {
-        setIsModalOpen(false)
-        dispatch(thunk.clearError())
+  const closeModal = (): void => {
+    setIsModalOpen(false);
+    dispatch(thunk.clearError());
+  };
+  useEffect(() => {
+    if (error) {
+      setIsModalOpen(true);
     }
-    useEffect(() => {
-      if (error) {
-          setIsModalOpen(true)
-      }
-      if(user){
-        navigate('/')
-      }
-    }, [error,user])
-
-  
+    if (user) {
+      navigate('/');
+    }
+  }, [error, user]);
 
   return (
-    <div className='container-auth reg'>
-      <h2>Заполните поля для регистрации:</h2>
-      <form className='sign-up' onSubmit={onHandleSubmit}>
-        <label htmlFor='name'>
-          <input
-            type='text'
-            name='login'
-            placeholder='Name'
-            value={login}
-            onChange={(e) => {
-              setLogin(e.target.value);
-            }}
-          />
-        </label>
-        <br />
-        <label htmlFor='email'>
-          <input
-            type='email'
-            name='email'
-            placeholder='Email'
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-        </label>
-        <br />
-        <label htmlFor='password'>
-          <input
-            type='password'
-            name='password'
-            placeholder='password'
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </label>
-        <br />
-        <label htmlFor='password'>
-          <input
-            type='password'
-            name='password'
-            placeholder='confirm password'
-            value={checkPassword}
-            onChange={(e) => {
-              setCheckPassword(e.target.value);
-            }}
-          />
-        </label>
-        <br />
-        <div className='button-container'>
-          <button type='submit'>Registration</button>
-          <Link to='/auth' className='login-button'>
-            {/* Sign in */}
-          </Link>
+    <div className="container-auth reg">
+      <div className="ring">
+        <i className="--clr" />
+        <i className="--clr" />
+        <i className="--clr" />
+        <div className="formAuth">
+          <h2>Регистрация</h2>
+          <form className="sign-up" onSubmit={onHandleSubmit}>
+            <div className="inputBx">
+              <input
+                type="text"
+                name="login"
+                placeholder="Имя"
+                value={login}
+                onChange={(e) => {
+                  setLogin(e.target.value);
+                }}
+              />
+            </div>
+
+            <div className="inputBx">
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div className="inputBx">
+              <input
+                type="password"
+                name="password"
+                placeholder="пароль"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+            <div className="inputBx">
+              <input
+                type="password"
+                name="password"
+                placeholder="повторите пароль"
+                value={checkPassword}
+                onChange={(e) => {
+                  setCheckPassword(e.target.value);
+                }}
+              />
+            </div>
+            <div className="button-container">
+              <div className="inputBx">
+                <button type="submit">Registration</button>
+              </div>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
       <Modal children={error} isOpen={isModalOpen} onClose={closeModal} />
     </div>
-  )
+  );
 }
 
 export default RegistrationPage;
