082f167369d02ae436c7b983e3bd1a17ad0390e9
MaryFBakeer <mariabefrank@gmail.com>
2024-04-17 16:15:42 +0300

    r

--- client/src/components/Cocktails/CocktailPage.tsx
+++ client/src/components/Cocktails/CocktailPage.tsx
@@ -12,7 +12,7 @@ function CoctailPage(): JSX.Element {
   const { id } = useParams<{ id: string }>();
   const dispatch = useAppDispatch();
   const formula: CocktailFormula | undefined = useAppSelector((store) => store.cocktails.cocktail);
-  
+
   const navigate = useNavigate();
   useEffect(() => {
     if (id) {
@@ -20,28 +20,36 @@ function CoctailPage(): JSX.Element {
     }
   }, []);
   return (
-    <div className="cocktailCardPage">
-      <h1>{formula?.title}</h1>
-      <img src={formula?.img} alt={formula?.title} />
-      <h2>Описание</h2>
-      <div>{formula?.description}</div>
-      <h2>Рецепт</h2>
-      {formula?.Formulas.toSorted((a, b) => (a.order ?? 0) - (b.order ?? 0))?.map((el:Formula) => (
-        <div key={el.id}>
-          {el.Barware && <div>{el.Barware?.title}</div>}
-          {el.Drink && (
-            <div>
-              {el.Drink?.title}: {el.drinks_volume}мл
-            </div>
-          )}
-          {el.Ingredient && (
-            <div>
-              {el.Ingredient?.title}: {el.ingredient_volume} {el.Ingredient?.measure}
-            </div>
-          )}
-          {el.Tech && <div>{el.Tech?.title}</div>}
+    <div className="pageCard">
+      <div className="cocktailCardPage">
+        <h1>{formula?.title}</h1>
+        <div className='allCardInfo'>
+          <img className='imgCard' src={formula?.img} alt={formula?.title} />
+          <div className='infoCard'>
+            <h2>Описание</h2>
+            <div className='descriptionCard'>{formula?.description}</div>
+            <h2>Рецепт</h2>
+            {formula?.Formulas.toSorted((a, b) => (a.order ?? 0) - (b.order ?? 0))?.map(
+              (el: Formula) => (
+                <div key={el.id}>
+                  {el.Barware && <div>{el.Barware?.title}</div>}
+                  {el.Drink && (
+                    <div>
+                      {el.Drink?.title}: {el.drinks_volume}мл
+                    </div>
+                  )}
+                  {el.Ingredient && (
+                    <div>
+                      {el.Ingredient?.title}: {el.ingredient_volume} {el.Ingredient?.measure}
+                    </div>
+                  )}
+                  {el.Tech && <div>{el.Tech?.title}</div>}
+                </div>
+              ),
+            )}
+          </div>
         </div>
-      ))}
+      </div>
       <button onClick={() => navigate(-1)}>Назад</button>
     </div>
   );
