0cdca4cb5670537ea7a6c48f5703495ba1cf63c3
LebedevD174 <Lebedevdima174@yandex.ru>
2024-04-18 14:55:21 +0300

    correct seeds

--- server/db/seeders/20240411151341-formulas.js
+++ server/db/seeders/20240411151341-formulas.js
@@ -14,26 +14,37 @@ module.exports = {
     */
    const cocktail1 = [
     {
-      barware_id: 5,
-    },
-    {
-      ingredient_id: 3,
-      ingredient_volume: 1,
-    },
-    {
-      drink_id: 1,
-      drinks_volume: 60,
-    },
-    {
-      drink_id: 6,
-      drinks_volume: 30,
-    },
-    {
-      tech_id: 1,
+      barware_id: 14,
     },
     {
       ingredient_id: 1,
-      ingredient_volume: 1,
+      ingredient_volume: 3,
+    },
+    {
+      ingredient_id: 2,
+      ingredient_volume: 5,
+    },
+    {
+      tech_id: 5,
+    },
+    {
+      ingredient_id: 3,
+      ingredient_volume: 200,
+    },
+    {
+      drink_id: 1,
+      drinks_volume: 15,
+    },
+    {
+      drink_id: 3,
+      drinks_volume: 60,
+    },
+    {
+      drink_id: 2,
+      drinks_volume: 100,
+    },
+    {
+      tech_id: 4,
     },
    ].map((el, i) => ({
     ...el,
@@ -44,26 +55,26 @@ module.exports = {
   }))
   const cocktail2 = [
     {
-      barware_id: 6,
+      barware_id: 16,
     },
     {
-      ingredient_id: 4,
-      ingredient_volume: 2,
+      ingredient_id: 3,
+      ingredient_volume: 8,
     },
     {
-      drink_id: 2,
-      drinks_volume: 70,
+      drink_id: 9,
+      drinks_volume: 50,
     },
     {
-      drink_id: 6,
-      drinks_volume: 35,
+      drink_id: 10,
+      drinks_volume: 50,
     },
     {
-      tech_id: 2,
+      drink_id: 11,
+      drinks_volume: 50,
     },
     {
-      ingredient_id: 2,
-      ingredient_volume: 2,
+      tech_id: 4,
     },
 ].map((el, i) => ({
     ...el,
@@ -75,26 +86,30 @@ module.exports = {
 
 const cocktail3 = [
   {
-    barware_id: 1,
-  },
-  {
-    ingredient_id: 5,
-    ingredient_volume: 3,
-  },
-  {
-    drink_id: 3,
-    drinks_volume: 80,
-  },
-  {
-    drink_id: 2,
-    drinks_volume: 40,
-  },
-  {
-    tech_id: 3,
+    barware_id: 16,
   },
   {
     ingredient_id: 3,
-    ingredient_volume: 3,
+    ingredient_volume: 8,
+  },
+  {
+    drink_id: 12,
+    drinks_volume: 30,
+  },
+  {
+    drink_id: 13,
+    drinks_volume: 30,
+  },
+  {
+    drink_id: 14,
+    drinks_volume: 30,
+  },
+  {
+    tech_id: 4,
+  },
+  {
+    ingredient_id: 7,
+    ingredient_volume: 1,
   },
 ].map((el, i) => ({
   ...el,
@@ -106,27 +121,31 @@ const cocktail3 = [
 
 const cocktail4 = [
   {
-    barware_id: 2,
+    barware_id: 16,
   },
   {
-    ingredient_id: 5,
-    ingredient_volume: 4,
+    ingredient_id: 8,
+    ingredient_volume: 1,
   },
   {
-    drink_id: 4,
-    drinks_volume: 90,
+    drink_id: 15,
+    drinks_volume: 1,
   },
   {
-    drink_id: 5,
-    drinks_volume: 45,
+    tech_id: 5,
+  },
+  {
+    drink_id: 16,
+    drinks_volume: 60,
+  },
+  {
+    ingredient_id: 3,
+    ingredient_volume: 8,
   },
   {
     tech_id: 4,
   },
-  {
-    ingredient_id: 4,
-    ingredient_volume: 4,
-  },
+  
 ].map((el, i) => ({
   ...el,
   cocktail_id: 4,
@@ -137,26 +156,32 @@ const cocktail4 = [
 
 const cocktail5 = [
   {
-    barware_id: 3,
+    barware_id: 1,
   },
   {
-    ingredient_id: 5,
-    ingredient_volume: 5,
-  },
-  {
-    drink_id: 5,
-    drinks_volume: 100,
+    drink_id: 8,
+    drinks_volume: 30,
   },
   {
     drink_id: 1,
-    drinks_volume: 50,
+    drinks_volume: 15,
   },
   {
-    tech_id: 5,
+    drink_id: 3,
+    drinks_volume: 70,
   },
   {
-    ingredient_id: 5,
-    ingredient_volume: 5,
+    ingredient_id: 3,
+    ingredient_volume: 15,
+  },
+  {
+    tech_id: 1,
+  },
+  {
+    tech_id: 2,
+  },
+  {
+    barware_id: 15,
   },
 ].map((el, i) => ({
   ...el,
@@ -168,30 +193,123 @@ const cocktail5 = [
 
 const cocktail6 = [
   {
-    barware_id: 4,
+    barware_id: 14,
   },
   {
-    ingredient_id: 2,
-    ingredient_volume: 6,
+    ingredient_id: 3,
+    ingredient_volume: 10,
   },
   {
-    drink_id: 6,
-    drinks_volume: 110,
+    drink_id: 11,
+    drinks_volume: 60,
   },
   {
-    drink_id: 4,
-    drinks_volume: 55,
+    drink_id: 16,
+    drinks_volume: 150,
   },
   {
-    tech_id: 6,
+    tech_id: 4,
   },
   {
-    ingredient_id: 2,
+    ingredient_id: 1,
     ingredient_volume: 2,
   },
 ].map((el, i) => ({
   ...el,
   cocktail_id: 6,
+  order: i+1,
+  createdAt: new Date(),
+  updatedAt: new Date()
+}));
+
+const cocktail7 = [
+  {
+    barware_id: 14,
+  },
+  {
+    ingredient_id: 3,
+    ingredient_volume: 10,
+  },
+  {
+    drink_id: 10,
+    drinks_volume: 60,
+  },
+  {
+    drink_id: 16,
+    drinks_volume: 150,
+  },
+  {
+    tech_id: 4,
+  },
+  {
+    ingredient_id: 7,
+    ingredient_volume: 1,
+  },
+].map((el, i) => ({
+  ...el,
+  cocktail_id: 7,
+  order: i+1,
+  createdAt: new Date(),
+  updatedAt: new Date()
+}));
+
+const cocktail8 = [
+  {
+    barware_id: 16,
+  },
+  {
+    ingredient_id: 3,
+    ingredient_volume: 8,
+  },
+  {
+    drink_id: 9,
+    drinks_volume: 30,
+  },
+  {
+    drink_id: 10,
+    drinks_volume: 60,
+  },
+  {
+    tech_id: 4,
+  },
+].map((el, i) => ({
+  ...el,
+  cocktail_id: 8,
+  order: i+1,
+  createdAt: new Date(),
+  updatedAt: new Date()
+}));
+
+const cocktail9 = [
+  {
+    barware_id: 17,
+  },
+  {
+    ingredient_id: 3,
+    ingredient_volume: 10,
+  },
+  {
+    drink_id: 18,
+    drinks_volume: 100,
+  },
+  {
+    drink_id: 19,
+    drinks_volume: 100,
+  },
+  {
+    drink_id: 2,
+    drinks_volume: 20,
+  },
+  {
+    tech_id: 4,
+  },
+  {
+    ingredient_id: 7,
+    ingredient_volume: 1,
+  },
+].map((el, i) => ({
+  ...el,
+  cocktail_id: 9,
   order: i+1,
   createdAt: new Date(),
   updatedAt: new Date()
@@ -205,6 +323,9 @@ const cocktail6 = [
     ...cocktail4,
     ...cocktail5,
     ...cocktail6,
+    ...cocktail7,
+    ...cocktail8,
+    ...cocktail9,
   ], {});
   },
 
